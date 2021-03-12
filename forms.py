5f8a95a4ed68fef9c6ff70bf4fc76a69a0999afb
Ben Jacobson <jacobson.ben11@gmail.com>
2021-03-12 15:08:20 -0500

    Add form working

--- forms.py
+++ forms.py
@@ -1,1 +1,15 @@
 """Forms for adopt app."""
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, FloatField, SelectField
+from wtforms.validators import InputRequired, Optional, Email
+
+
+class AddPet(FlaskForm):
+    """Form for adding pets."""
+
+    name = StringField("Pet Name")
+    species = SelectField("Species", choices=[('cat', 'Cat'), ('dog', 'Dog'), ('porcupine', 'Porcupine')])
+    photo_url = StringField("Link to Photo")
+    age = SelectField("Age", choices=[('baby', 'baby'), ('young', 'Young'), ('adult', 'Adult'), ('senior', 'Senior')])
+    notes = StringField("Notes")
--- models.py
+++ models.py
@@ -13,3 +13,46 @@ def connect_db(app):
 
     db.app = app
     db.init_app(app)
+
+class Pet(db.Model):
+    """Pet"""
+
+    __tablename__ = "pets"
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True
+    )
+
+    name = db.Column(
+        db.String(50),
+        nullable=False
+    )
+
+    species = db.Column(
+        db.String(50),
+        nullable=False
+    )
+
+    photo_url = db.Column(
+        db.Text,
+        nullable=False,
+        default=''
+    )
+
+    age = db.Column(
+        db.Text,
+        nullable=False
+    )
+
+    notes = db.Column(
+        db.Text,
+        nullable=True
+    )
+
+    available = db.Column(
+        db.Boolean,
+        nullable=False,
+        default=True
+    )
